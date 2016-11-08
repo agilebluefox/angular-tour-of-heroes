8bdde47ea4621e21361ddd5601bbfa323a581ec2
David Conner <agilebluefox@gmail.com>
2016-11-07 22:15:47 -0500

    Prepare the HeroService to get the heroes list
    Import the Hero class definition and the heroes array. Add a getHeroes method that will return the array of heroes when called.

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 
+// Get the heroes array and the hero class definition
+import { Hero } from '../shared/hero';
+import { HEROES } from '../shared/mock-heroes';
+
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
 // Method to get the list of heroes
-  getHeroes(): void {
-
+  getHeroes(): Hero[] {
+    return HEROES;
   }
 
 }
