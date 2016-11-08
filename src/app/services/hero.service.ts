0ad126d08d823bfebca7bd945e9d4a293fb4e283
David Conner <agilebluefox@gmail.com>
2016-11-07 22:00:58 -0500

    Setup the base service to handle hero data
    To decouple the data access from the components that use it requires a service module. This service will be responsible for accessing the data and providing it upon request. This way the components  that use the data are not concerned with how the data is accessed  or where the data originates.

---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+// Method to get the list of heroes
+  getHeroes(): void {
+
+  }
+
+}
