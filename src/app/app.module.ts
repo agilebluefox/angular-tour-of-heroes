e39f1d241307050a893850e649f34ef2970ae3b6
David Conner <agilebluefox@gmail.com>
2016-11-08 14:19:56 -0500

    Relocate the hero service provider
    Since the hero service will be needed in multiple components, it's best to add it to the providers array in the app module, rather than having to add it to the providers array in each of the components that use it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { HeroService } from './services/hero.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailsComponent } from './heroes/hero-details/hero-details.component';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailsComponent
+    HeroDetailsComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
