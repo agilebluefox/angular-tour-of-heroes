80c73aed06135e87d1395ba4eb8b09f9a47dea6f
David Conner <agilebluefox@gmail.com>
2016-11-07 21:37:05 -0500

    Add input decorator to hero property
    One method to declare that a property is the target of a binding is to use the @Input decorator as an annotation above the property.

--- src/app/heroes/hero-details/hero-details.component.ts
+++ src/app/heroes/hero-details/hero-details.component.ts
@@ -8,7 +8,10 @@ import { Hero } from '../../shared/hero';
   styleUrls: ['./hero-details.component.css']
 })
 export class HeroDetailsComponent implements OnInit {
-
+  // This property needs to come from the AppComponent.
+  // It must be an input property since it's the target
+  // of the assignment from the selectedHero in the AppComponent.
+  @Input() // Decorator for input properties
   hero: Hero;
 
   constructor() { }
