67f9645a6dfa8587ebcbe198ac298a2f2d3efd80
David Conner <agilebluefox@gmail.com>
2016-11-07 22:23:22 -0500

    Inject the HeroService into the app
    Start by importing the HeroService, adding it to the list of providers, and creating an instance of the service in the class constructor as a private variable. Angular will take care of making sure an instance of the service is present.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './shared/hero';
+import { HeroService } from './services/hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
 export class AppComponent {
+  constructor (private heroService: HeroService) {}
   // Add a title property
   title = 'Tour of Heroes';
   // Add a selected hero property
