84a9ec3d5b50e6fbffd934310e796293c4ce8827
David Conner <agilebluefox@gmail.com>
2016-11-07 19:41:53 -0500

    Add a hero class
    This seems like an Interface object which defines the properties that all objects created from it must contain.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+// Create a hero class
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  // Add a title property
   title = 'Tour of Heroes';
+  // Add the hero property
   hero = 'Windstorm';
 }
