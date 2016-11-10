fc932b2cfcba111e71419e4c558bdf69c712da75
David Conner <agilebluefox@gmail.com>
2016-11-10 17:02:21 -0500

    Add the search execution code
    This is where it gets really confusing. There will be multiple calls to the api and multiple arrays of heroes being returned based on the search term.

--- src/app/heroes/hero-search/hero-search.component.ts
+++ src/app/heroes/hero-search/hero-search.component.ts
@@ -14,14 +14,35 @@ import { Hero } from '../../shared/hero';
 })
 export class HeroSearchComponent implements OnInit {
   heroes: Observable<Hero[]>;
-
+  private searchTerms = new Subject<string>();
 
   constructor(
     private heroSearchService: HeroSearchService,
     private router: Router
   ) { }
 
-  ngOnInit() {
+  // Push a search term into the observable stream
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes = this.searchTerms
+      .debounceTime(300)  // wait for 300ms pause in events
+      .distinctUntilChanged() // ignore if the search term hasn't changed
+      .switchMap(term => term // switch to the new observable each time
+        ? this.heroSearchService.search(term)
+        : Observable.of<Hero[]>([]))
+      .catch(error => {
+        console.log(error);
+        return Observable.of<Hero[]>([]);
+      });
+  }
+
+  // Navigate to the details page if the hero is clicked
+  goToDetails(hero: Hero): void {
+    let link = ['/details', hero.id];
+    this.router.navigate(link);
   }
 
 }
